1f4e00688cf9a7642f28c461b880eeb675715187
Quiteblank <iamquiteblank@gmail.com>
2016-08-20 21:14:24 -0700

    Angular application with basic CRUD operations.

--- RDLCReport/App_Start/BundleConfig.cs
+++ RDLCReport/App_Start/BundleConfig.cs
@@ -9,23 +9,29 @@ namespace RDLCReport
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/ui/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/ui/bootstrap.js",
+                      "~/Scripts/ui/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angularjs").Include(
+                        "~/Scripts/angular/angular.js"));
+            
+            bundles.Add(new ScriptBundle("~/bundles/appjs").Include(
+                        "~/Scripts/app/customerCtrl.js"));
         }
     }
 }
